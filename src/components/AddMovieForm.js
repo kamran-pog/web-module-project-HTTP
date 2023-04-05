7473996f56153167d406b264c8542a1d3dddb7d9
kamran-pog <kamran.k0807@gmail.com>
2023-04-05 14:27:43 -0700

    MVP

--- src/components/AddMovieForm.js
+++ src/components/AddMovieForm.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import {useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 import axios from 'axios';
 
-const EditMovieForm = (props) => {
+const AddMovieForm = (props) => {
   const navigate = useNavigate();
-  const { id } = useParams();
   const { setMovies } = props;
+
+
   const [movie, setMovie] = useState({
     title: "",
     director: "",
@@ -25,10 +26,11 @@ const EditMovieForm = (props) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.put(`http://localhost:9000/api/movies/${id}`, movie)
+    axios.post(`http://localhost:9000/api/movies`, movie)
       .then(res => {
-        setMovies(res.data);
-        navigate(`/movies/${movie.id}`);
+
+        props.setMovies(res.data);
+        navigate(`/movies`);
       })
       .catch(err => {
         console.log(err);
@@ -42,7 +44,7 @@ const EditMovieForm = (props) => {
       <div className="modal-content">
         <form onSubmit={handleSubmit}>
           <div className="modal-header">
-            <h4 className="modal-title">Editing <strong>{movie.title}</strong></h4>
+            <h4 className="modal-title">Adding<strong>{movie.title}</strong></h4>
           </div>
           <div className="modal-body">
             <div className="form-group">
@@ -69,11 +71,11 @@ const EditMovieForm = (props) => {
           </div>
           <div className="modal-footer">
             <input type="submit" className="btn btn-info" value="Save" />
-            <Link to={`/movies/1`}><input type="button" className="btn btn-default" value="Cancel" /></Link>
+            <Link to={`/movies`}><input type="button" className="btn btn-default" value="Cancel" /></Link>
           </div>
         </form>
       </div>
     </div>);
 }
 
-export default EditMovieForm;
+export default AddMovieForm;
